938b838546cf774dc8841000996552cef52cccf3
laksjdjf <aikasama@hotmail.co.jp>
2023-11-23 11:06:20 +0900

    Update negpip.py

--- negpip.py
+++ negpip.py
@@ -1,6 +1,6 @@
 import torch
 import copy
-
+from comfy.sd1_clip import gen_empty_tokens
 
 class Negpip:
     @classmethod
@@ -39,45 +39,53 @@ class Negpip:
 # k,vはattn2_patchで分離する
 # weightがマイナスのときはvだけマイナスにする
 def hook_clip_encode_token_weights(self):
+    
     def encode_token_weights(token_weight_pairs):
-        to_encode = list(self.empty_tokens)
+        to_encode = list()
+        max_token_len = 0
+        has_weights = False
         for x in token_weight_pairs:
             tokens = list(map(lambda a: a[0], x))
+            max_token_len = max(len(tokens), max_token_len)
+            has_weights = has_weights or not all(map(lambda a: a[1] == 1.0, x))
             to_encode.append(tokens)
 
+        sections = len(to_encode)
+        if has_weights or sections == 0:
+            to_encode.append(gen_empty_tokens(self.special_tokens, max_token_len))
+
         out, pooled = self.encode(to_encode)
-        z_empty = out[0:1]
-        if pooled.shape[0] > 1:
-            first_pooled = pooled[1:2]
+        if pooled is not None:
+            first_pooled = pooled[0:1].cpu()
         else:
-            first_pooled = pooled[0:1]
+            first_pooled = pooled
 
         output = []
-
-        for k in range(1, out.shape[0]):
+        for k in range(0, sections):
             zk = out[k:k+1].clone()
             zv = out[k:k+1].clone()
-            for i in range(len(zk)):
-                for j in range(len(zk[i])):
-                    weight = token_weight_pairs[k - 1][j][1]
-                    if weight < 0:
-                        weight = -weight
-                        sign = -1
-                    else:
-                        sign = 1
-                    zk[i][j] = (zk[i][j] - z_empty[0][j]) * weight + z_empty[0][j]
-                    zv[i][j] = sign * ((zv[i][j] - z_empty[0][j]) * weight + z_empty[0][j])
+            if has_weights:
+                z_empty = out[-1]
+                for i in range(len(zk)):
+                    for j in range(len(zk[i])):
+                        weight = token_weight_pairs[k][j][1]
+                        if weight < 0:
+                            weight = -weight
+                            sign = -1
+                        else:
+                            sign = 1
+                        zk[i][j] = (zk[i][j] - z_empty[0][j]) * weight + z_empty[0][j]
+                        zv[i][j] = sign * ((zv[i][j] - z_empty[0][j]) * weight + z_empty[0][j])
 
             z = torch.zeros_like(zk).repeat(1, 2, 1)
             for i in range(zk.shape[1]):  # 頭悪いのでfor文
                 z[:, 2*i, :] += zk[:, i, :]
                 z[:, 2*i+1, :] += zv[:, i, :]
-
             output.append(z)
 
         if (len(output) == 0):
-            return z_empty.cpu(), first_pooled.cpu()
-        return torch.cat(output, dim=-2).cpu(), first_pooled.cpu()
+            return out[-1:].cpu(), first_pooled
+        return torch.cat(output, dim=-2).cpu(), first_pooled
 
     return encode_token_weights
 
